a7e90c1778124569cf2291a6ebd3efc36b70af01
Sougata Nayak <sougatanayak007@gmail.com>
2020-04-11 22:27:36 +0530

    Commiting the game files

---
+++ CubeKing/Assets/Scripts/PlayerCollisionScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerCollisionScript : MonoBehaviour
+{
+    public PlayerMovementScript movement;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag.Equals("Obstacles"))
+        {
+            Debug.Log(collision.collider.name);
+
+            movement.enabled = false;
+
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
---
+++ CubeKing/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    public float forwardForce = 1500f;
+    public float sidewaysForce = 500f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Game started");
+
+        rigidbody.useGravity = true;
+    }
+
+    // Update is called once per frame
+    // We are using FixedUpdate rather than Update cuz it helps Unity for messing with physics
+    void FixedUpdate()
+    {
+        rigidbody.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rigidbody.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        else if (Input.GetKey("a"))
+        {
+            rigidbody.AddForce(sidewaysForce * -1 * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if(rigidbody.position.y < -1)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
